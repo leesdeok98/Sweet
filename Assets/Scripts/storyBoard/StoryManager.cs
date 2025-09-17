485268216fe0a227a7e3cede8d0401a7fb05097d
나무냥 <treecatt0@naver.com>
2025-09-17 18:37:06 +0900

    20250917 남은영
    Scenes->StoryBoard : image, text 사이즈, 색상 변경
    
    Scripts->StoryManager : 스페이스바 누를 때마다 이미지 넘어가는 거 구현 (텍스트x)

--- Assets/Scripts/storyBoard/StoryManager.cs
+++ Assets/Scripts/storyBoard/StoryManager.cs
@@ -1,4 +1,4 @@
-/* using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -15,27 +15,26 @@ public class StoryManager : MonoBehaviour
     [Header("스크립터블")]
     [SerializeField] private ChangeImageText[] images;
 
-    private int currentImage = 0;   // 첫 번째 이미지로 시작
+    private int currentImage = 1;   // 두 번째 이미지부터 시작 (첫 번째 이미지는 Source image에 넣고 시작)
 
-    
     void Start()
     {
-        ShowImage();
+        //text.text = "";
+        //image.sprite = images[0].StoryImage;  //Start에 넣었을 때 사진이 느리게 떠서 Source image에 넣음
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            currentImage++;
-
-            ShowImage();
+            ShowNextImage();
         }
     }
 
-    void ShowImage()
+    void ShowNextImage()
     {
         image.sprite = images[currentImage].StoryImage;
-        text.text = images[currentImage].StoryText;
+        currentImage++;
     }
-} */
+
+}
