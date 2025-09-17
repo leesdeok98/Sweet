fd5176b2b52ee1d4fd0f47dd539eda1e527c6003
moondodo <yeona6510@naver.com>
2025-09-18 00:58:32 +0900

    문연화
    SoundUIManager 스크립트 생성 -> 전체 Slider 볼륨 값 50 설정 / (PlayerPrefs) Slider 볼륨 값 저장 시스템 구현
    
    SoundUIManager 오브젝트 생성 -> SoundUIManager 스크립트 연결

---
+++ Assets/Scripts/SoundUIManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundUIManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    public void Start()
+    {
+        float master = PlayerPrefs.GetFloat("Master", 0.50005f);
+        float bgm = PlayerPrefs.GetFloat("BGM", 0.50005f);
+        float sfx = PlayerPrefs.GetFloat("SFX", 0.50005f);
+
+        masterSlider.value = master;
+        bgmSlider.value = bgm;
+        sfxSlider.value = sfx;
+
+        SetMasterVolume(master);
+        SetBGMVolume(bgm);
+        SetSFXVolume(sfx);
+
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("Master", value);
+        PlayerPrefs.Save();
+    }
+
+    public void SetBGMVolume(float value)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("BGM", value);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFX", value);
+        PlayerPrefs.Save();
+    }
+}
