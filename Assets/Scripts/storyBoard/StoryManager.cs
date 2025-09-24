86aae2dd2661e6a3072d7f3bbb6ea759c97042be
나무냥 <treecatt0@naver.com>
2025-09-25 06:00:56 +0900

    남은영
    이미지 -> 텍스트(타이핑 효과) -> 다음 이미지 -> 다음 텍스트(타이핑 효과) : 구현
    
    *타이핑 효과가 나오는 도중에 스페이스 바를 눌러도 다음 이미지로 넘어가지 않게 구현*

--- Assets/Scripts/storyBoard/StoryManager.cs
+++ Assets/Scripts/storyBoard/StoryManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEditor.Experimental.GraphView;
 
 public class StoryManager : MonoBehaviour
 {
@@ -15,25 +16,64 @@ public class StoryManager : MonoBehaviour
     [Header("스크립터블")]
     [SerializeField] private ChangeImageText[] images;
 
-    private int currentImage = 1;   // 두 번째 이미지부터 시작 (첫 번째 이미지는 Source image에 넣고 시작)
+    [Header("타이핑 효과")]
+    [SerializeField] private float TypingSpeed = 0.03f;
+
+    private int currentImage = 0; 
+    private bool isTyping = false;
+    private bool isImageChanging = true;
+    private Coroutine TypingCorutine;
 
     void Start()
     {
         //text.text = "";
-        //image.sprite = images[0].StoryImage;  //Start에 넣었을 때 사진이 느리게 떠서 Source image에 넣음
+        image.enabled = false;   // 이미지 안 보이게 꺼두기
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ShowNextImage();
+            if (isImageChanging)   //이미지 바꾸기
+            {
+                text.text = "";   //새로운 이미지로 넘어갔을 때 텍스트 비우기
+                ShowNextImage();
+                isImageChanging = false;
+            }
+            
+            else if (isTyping)
+            {
+                return;
+            }
+
+            else   //타이핑 효과
+            {
+                text.text = "";     //텍스트 초기화
+                TypingCorutine = StartCoroutine(WriteText(images[currentImage].StoryText));  //타이핑 시작
+                isTyping = true;    //상태변경
+                isImageChanging = true; 
+            }
         }
     }
 
     void ShowNextImage()
     {
+        image.enabled = true; //이미지 보이게 하기
         image.sprite = images[currentImage].StoryImage;
+        
+    }
+
+    private IEnumerator WriteText(string StoryText)
+    {
+        // 타이핑 효과 적용
+        for (int i = 0; i <= StoryText.Length; i++)
+        {
+            text.text = StoryText.Substring(0, i);
+            yield return new WaitForSeconds(TypingSpeed);
+        }
+
+        // 타이핑 완료
+        isTyping = false;
         currentImage++;
     }
 
